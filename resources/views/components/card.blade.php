5e2da6af2881438b0965c1d78fbc15d7be2f97b6
paizay24 <paizay.dev24@gmail.com>
2024-06-19 19:23:12 +0630

    jobs_employers_tags

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,16 +1,18 @@
+@props(['job'])
+
 <x-panel>
-    <div class="self-start text-sm">Laracasts</div>
+    <div class="self-start text-sm">{{ $job->employer->name }}</div>
 
     <div class="py-8 font-bold">
-        <h3 class=" font-bold text-xl mt-3 group-hover:text-blue-800 duration-150">Video Producer</h3>
-        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
+        <h3 class=" font-bold text-xl mt-3 group-hover:text-blue-800 duration-150">{{ $job->title }}</h3>
+        <p class="text-sm text-gray-400 mt-auto">{{ $job->salary }}</p>
     </div>
 
     <div class="flex justify-between items-center mt-auto">
         <div>
-            <x-tag size="small">Tag</x-tag>
-            <x-tag size="small">Tag</x-tag>
-            <x-tag size="small">Tag</x-tag>
+            @foreach ($job->tags as $tag )
+                <x-tag :$tag></x-tag>
+            @endforeach
         </div>
         <x-employer-logo width='42'></x-employer-logo>
      </div>
---
+++ resources/views/jobs/index.blade.php
@@ -0,0 +1,38 @@
+<x-layout>
+    <div class=" space-y-6">
+        <section class="text-center pt-6">
+            <h1 class="font-bold text-4xl">Let's Find Your Next Job</h1>
+
+            <form action="" class="mt-6">
+                <input type="text" placeholder="Web Developer..." class="rounded-xl bg-white/5 border-white/10 px-5 py-4 w-full max-w-xl">
+            </form>
+        </section>
+        <section>
+            <x-section-heading class=" mb-5">Feature Jobs</x-section-heading>
+            <div class=" grid lg:grid-cols-3 gap-8">
+                @foreach ( $featuredJobs as $job )
+                    <x-card :$job></x-card>
+                @endforeach
+
+            </div>
+        </section>
+        <section>
+            <x-section-heading class=" mb-5">Tags</x-section-heading>
+            <div class=" mt-6">
+                @foreach ($tags as $tag )
+                    <x-tag :$tag></x-tag>
+                @endforeach
+
+            </div>
+        </section>
+        <section class=" mt-10 space-y-3">
+            <x-section-heading class=" mb-5">Recent Jobs</x-section-heading>
+
+            @foreach ($jobs as $job )
+                <x-card-wide :$job></x-card-wide>
+            @endforeach
+        </section>
+    </div>
+
+
+</x-layout>
